272179836d581300079bdded96689449d51210a6
Micha Lenk <micha@debian.org>
2011-07-13 22:21:22 +0200

    Imported Upstream version 2.4.7

--- src/scm/build-config.scm
+++ src/scm/build-config.scm
@@ -1,6 +1,6 @@
 
-(define gnc:version "2.4.6")
+(define gnc:version "2.4.7")
 
 ;; Automatically generated defaults (don't use these directly --
 ;; they're used during actual initialization elsewhere)
-(define gnc:_install-doc-path_ '("/home/cs/usr-2.4/share"))
+(define gnc:_install-doc-path_ '("/usr/local/share"))
