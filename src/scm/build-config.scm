19fd8e84a5b87dc1f45077651f13c20a2e2c6822
Dmitry Smirnov <onlyjob@member.fsf.org>
2015-04-29 10:46:17 +1000

    Added "debian/gbp.conf"; removed upstream files from master

--- src/scm/build-config.scm
+++
@@ -1,6 +0,0 @@
-
-(define gnc:version "2.6.4")
-
-;; Automatically generated defaults (don't use these directly --
-;; they're used during actual initialization elsewhere)
-(define gnc:_install-doc-path_ '("/opt/gnucash/share"))
