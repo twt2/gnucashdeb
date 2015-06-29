44fa08fe75eb267cf4579d4978ce7bcca6e0a268
Dmitry Smirnov <onlyjob@member.fsf.org>
2015-06-29 19:52:08 +1000

    Imported Upstream version 2.6.7

--- src/scm/build-config.scm
+++ src/scm/build-config.scm
@@ -1,6 +1,6 @@
 
-(define gnc:version "2.6.6")
+(define gnc:version "2.6.7")
 
 ;; Automatically generated defaults (don't use these directly --
 ;; they're used during actual initialization elsewhere)
-(define gnc:_install-doc-path_ '("/usr/local/share"))
+(define gnc:_install-doc-path_ '("/opt/share"))
