8962d68969cab17debc2a2dff756072df2f707f2
Dmitry Smirnov <onlyjob@member.fsf.org>
2018-03-14 15:48:33 +1100

    New upstream version 2.7.6

--- src/scm/build-config.scm
+++
@@ -1,6 +0,0 @@
-
-(define gnc:version "2.6.18")
-
-;; Automatically generated defaults (don't use these directly --
-;; they're used during actual initialization elsewhere)
-(define gnc:_install-doc-path_ '("/opt/gnucash/share"))
